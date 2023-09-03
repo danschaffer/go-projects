ba132233f803a626bddaf51e50ba40b9a0f59113
Dan Schaffer <dschaffer@circle.com>
2023-09-02 21:19:29 -0400

    if/else conditions and block statements

--- interpreter/ast/ast.go
+++ interpreter/ast/ast.go
@@ -50,6 +50,18 @@ type ReturnStatement struct {
 	ReturnValue Expression
 }
 
+type IfExpression struct {
+	Token       token.Token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+type BlockStatement struct {
+	Token      token.Token
+	Statements []Statement
+}
+
 type ExpressionStatement struct {
 	Token      token.Token
 	Expression Expression
@@ -66,6 +78,41 @@ type InfixExpression struct {
 	Left     Expression
 	Operator string
 	Right    Expression
+}
+
+func (bs *BlockStatement) statementNode() {
+}
+
+func (bs *BlockStatement) TokenLiteral() string {
+	return bs.Token.Literal
+}
+
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
+func (ie *IfExpression) expressionNode() {
+}
+
+func (ie *IfExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+	return out.String()
 }
 
 func (b *Boolean) expressionNode() {
