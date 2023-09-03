5f57a05c0eb8b759232a1e2c8f31148069c89daf
Dan Schaffer <dschaffer@circle.com>
2023-09-03 15:44:01 -0400

    functions support, add parse to repl

--- interpreter/ast/ast.go
+++ interpreter/ast/ast.go
@@ -3,7 +3,20 @@ package ast
 import (
 	"bytes"
 	"monkey/token"
+	"strings"
 )
+
+type CallExpression struct {
+	Token     token.Token
+	Function  Expression
+	Arguments []Expression
+}
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
 
 type Node interface {
 	TokenLiteral() string
@@ -78,6 +91,47 @@ type InfixExpression struct {
 	Left     Expression
 	Operator string
 	Right    Expression
+}
+
+func (ce *CallExpression) expressionNode() {
+}
+
+func (ce *CallExpression) TokenLiteral() string {
+	return ce.Token.Literal
+}
+
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+	args := []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+	return out.String()
+}
+
+func (fl *FunctionLiteral) expressionNode() {
+}
+
+func (fl *FunctionLiteral) TokenLiteral() string {
+	return fl.Token.Literal
+}
+
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+	return out.String()
 }
 
 func (bs *BlockStatement) statementNode() {
